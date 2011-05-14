caa7209ec523c342657b29e09502d5760d7ac84b
David Young <yangboh@cn.ibm.com>
2011-05-14 22:55:21 +0800

    Refine format

--- settings.el
+++ settings.el
@@ -10,35 +10,46 @@
 ;(ruler-mode -1)
 (setq system-time-locale "C")
 
-(setq save-abbrevs t
-      x-select-enable-clipboard t
-      ispell-dictionary "english"
-      frame-title-format "%b %n %I"
-      inhibit-startup-message t
-      column-number-mode t
-      show-paren-style 'mixed
-      default-fill-column 100
-      kill-do-not-save-duplicates t
-      uniquify-buffer-name-style 'forward
-      paragraph-start "\\*\\|$" 
-      paragraph-separate "$"
-      todo-file-do "~/todo/do"
-      todo-file-done "~/todo/done"
-      todo-file-top "~/todo/top"
-      appt-issue-message t
-      calendar-latitude +39.54
-      calendar-longitude +116.23
-      calendar-location-name "Beijing"
-      mark-diary-entries-in-calendar t
-      display-time-day-and-date t
-      ibuffer-default-sorting-mode 'major-mode
-      ido-toggle-regexp t
-      multi-term-program "/bin/bash"
-      dim:switch-window-relative nil
-      shell-file-name "/bin/bash"
-      default-major-mode 'text-mode
-      cperl-xemacs-p nil
-;      dired-isearch-filenames dwim
+(display-time)
+(bbdb-initialize)
+(global-visual-line-mode 1)
+(tabbar-mode)
+;(icy-mode 1)
+(read-abbrev-file "~/.abbrev_defs")
+(mouse-avoidance-mode 'jump)
+(auto-image-file-mode)
+(show-paren-mode t)
+
+
+(setq save-abbrevs                                       t
+      x-select-enable-clipboard                          t
+      inhibit-startup-message                            t
+      column-number-mode                                 t
+      mark-diary-entries-in-calendar                     t
+      display-time-day-and-date                          t
+      ido-toggle-regexp                                  t
+      kill-do-not-save-duplicates                        t
+      appt-issue-message                                 t
+      show-paren-style                                   'mixed
+      default-fill-column                                100
+      uniquify-buffer-name-style                         'forward
+      ispell-dictionary                                  "english"
+      frame-title-format                                 "%b %n %I"
+      paragraph-start                                    "\\*\\|$" 
+      paragraph-separate                                 "$"
+      todo-file-do                                       "~/todo/do"
+      todo-file-done                                     "~/todo/done"
+      todo-file-top                                      "~/todo/top"
+      calendar-latitude                                  +39.54
+      calendar-longitude                                 +116.23
+      calendar-location-name                             "Beijing"
+      ibuffer-default-sorting-mode                       'major-mode
+      multi-term-program                                 "/bin/bash"
+      dim:switch-window-relative                         nil
+      shell-file-name                                    "/bin/bash"
+      default-major-mode                                 'text-mode
+      cperl-xemacs-p                                     nil
+;      dired-isearch-filenames                           dwim
 )
 
 ; hippie mode remember an anchor then jump back
@@ -65,7 +76,7 @@
 
 ; http://www.emacswiki.org/emacs-zh/FillParagraph
 
-;(setq paragraph-start "\\*\\|$" 
+ ;(setq paragraph-start "\\*\\|$" 
 ;          paragraph-separate "$")
 ; http://www.emacswiki.org/emacs-zh/AutoFillMode
 ;;    (add-hook 'text-mode-hook
