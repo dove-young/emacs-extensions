1afe25c3eaf7785013f1c80453958c3a5a003bb4
David Young <yangboh@cn.ibm.com>
2011-06-04 15:32:30 +0800

    customize w3m key binding; fix icicles conflict with orgmode babel; move fun code to zolu.el

--- plugins.el
+++ plugins.el
@@ -69,9 +69,14 @@
 
 (eval-after-load 'w3m
   '(progn
-     (define-key w3m-mode-map "h" 'w3m-previous-buffer)
-     (define-key w3m-mode-map "l" 'w3m-next-buffer)
-     (define-key w3m-mode-map (kbd "C-w") 'w3m-close-window)))
+     (add-hook 'w3m-mode-hook (lambda ()
+               (define-key w3m-mode-map "e" (lambda nil (interactive) (w3m-scroll-up-1 (/ (window-height) 2))))  
+                                        ; (w3m-linknum-edit-this-url)
+               (define-key w3m-mode-map "o" (lambda nil (interactive) (w3m-scroll-up-1 (- 0 (/ (window-height) 2)))))
+               (define-key w3m-mode-map "h" 'w3m-previous-buffer)
+               (define-key w3m-mode-map "l" 'w3m-next-buffer)
+               (define-key w3m-mode-map "l" 'w3m-next-buffer)
+               (define-key w3m-mode-map (kbd "C-w") 'w3m-close-window)))))
 
 (add-hook 'emacs-lisp-mode-hook 
           (lambda ()
@@ -154,6 +159,18 @@
             (define-key view-mode-map "j" 'View-scroll-page-forward)
             (define-key view-mode-map "k" 'View-scroll-page-backward)
 ))
+
+(eval-after-load "icicles-opt.el"
+ (add-hook 'icicle-mode-hook
+          (lambda ()
+             (setq my-icicle-top-level-key-bindings
+                     (mapcar (lambda (lst)
+                               (unless (string= "icicle-occur" (nth 1 lst)) lst))
+                              icicle-top-level-key-bindings))
+                    (setq icicle-top-level-key-bindings my-icicle-top-level-key-bindings) )))
+
+(icy-mode)
+;)
 
 ; (setq view-mode-hook nil)
 ; o was bound to (View-scroll-to-buffer-end)
@@ -254,57 +271,6 @@ that was stored with ska-point-to-register."
         (set-register 8 tmp)))
 
 
-;; ====================================================================
-;; ==================== 99 bottle of beers ============================
-;; ====================================================================
-
-; The following code is copied from http://99-bottles-of-beer.net/
-; I just love it. 
-
-(defun beer-text (num &optional capitalise)
-  "Returns a phrase for the given number of bottles of beer.
-The phrase is colour-coded red if there are no bottles left or
-blue otherwise. If the optional argument CAPITALISE is non-nil,
-the first letter of the phrase is capitalised."
-  (propertize (cond ((= num 0) (concat (if capitalise "N" "n")
-				       "o more bottles of beer"))
-		    ((= num 1) "1 bottle of beer")
-		    (t         (format "%i bottles of beer" num)))
-	      'face (list :foreground (if (equal num 0) "red" "blue"))))
-
-(defun beer-verse (num)
-  "Returns a complete verse for the given number of bottles."
-  (concat (beer-text num t) " on the wall, "
-	  (beer-text num) ".\n"
-	  (if (equal num 0)
-	      (concat "Go to the store and buy some more, "
-		      (beer-text 99) " on the wall.\n")
-	    (concat "Take one down and pass it around, "
-		    (beer-text (1- num)) " on the wall.\n"))))
-
-(defun beer-song-insert (start)
-  "Inserts the complete 99 bottles of beer song into the buffer."
-  (let ((n start))
-    (while (>= n 0)
-      (insert (beer-verse n))
-      (when (>= (setq n (1- n)) 0)
-	(insert "\n")))))
-
-(defun beer-song (start)
-  "Show the 99 bottles of beer song.
-The song is created in a buffer called *Beer song* and the buffer
-is displayed. If a prefix is given, the song is started from that
-number of bottles, otherwise it starts from 99."
-  (interactive "P")
-  (let ((buf (get-buffer-create "*Beer song*")))
-    (with-current-buffer buf
-      (kill-all-local-variables)
-      (erase-buffer)
-      (beer-song-insert (if start (prefix-numeric-value start) 99))
-      (goto-char (point-min)))
-    (view-buffer buf)))
-
-
     (defun find-next-occurance-of-region (start end)
       "Jump to the next occurance of region, and sets it as the current region"
       (interactive "r")
@@ -318,27 +284,5 @@ number of bottles, otherwise it starts from 99."
 
 ;=============================================================================
 ;=============================================================================
-
-
-
-;(defun factorial(n) 
-;  (if (= n 1) 
-;      1  
-;    (* n (factorial (- n 1)))
-;      )
-;)
-
-;(message "%d" (factorial 6))
-
-
-
-;		 (local-set-key "(" 'skeleton-pair-insert-maybe)
-;		 (local-set-key "[" 'skeleton-pair-insert-maybe)
-;		 (local-set-key "{" 'skeleton-pair-insert-maybe)
-
-
-;    (require 'session)
-
-
 ;(load "zeitgeist.el")
 
