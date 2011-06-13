040fc0614262cfe24c5d5f8e1bf0c0ba83901221
David Young <yangboh@cn.ibm.com>
2011-06-13 14:26:37 +0800

    Fixed change-split-type-3

--- dove-ext.el
+++ dove-ext.el
@@ -608,31 +608,36 @@ When used in shell-mode, it will paste parenthesis on shell prompt by default "
 
 (defun change-split-type-3 ()
   "Change 3 window style from horizontal to vertical and vice-versa"
-  (interactive "p")
+  (interactive)
   (select-window (get-largest-window))
   (if (= 3 (length (window-list)))
       (let ((winList (window-list)))
-	    (let ((1stBuf (window-buffer (car winList)))
-		  (2ndBuf (window-buffer (car (cdr winList))))
-		  (3rdBuf (window-buffer (car (cdr (cdr winList)))))
-		  (split-3 
-		   (lambda(1stBuf 2ndBuf 3rdBuf split-1 split-2)
-		     "change 3 window from horizontal to vertical and vice-versa"
-		     (message "%s %s %s" 1stBuf 2ndBuf 3rdBuf)
-		     (delete-other-windows)
-		     (funcall split-1)
-		     (set-window-buffer nil 2ndBuf)
-		     (funcall split-2)
-		     (set-window-buffer (next-window) 3rdBuf)
-		     (other-window 2)
-		     (set-window-buffer nil 1stBuf)))
-		  (split-type-1 nil)
-		  (split-type-2 nil)
-		  )
-	      (if (= (window-width) (frame-width))
-		  (setq split-type-1 'split-window-horizontally split-type-2 'split-window-vertically)
-		(setq split-type-1 'split-window-vertically  split-type-2 'split-window-horizontally))
-	      (funcall split-3 1stBuf 2ndBuf 3rdBuf split-type-1 split-type-2)
+            (let ((1stBuf (window-buffer (car winList)))
+                  (2ndBuf (window-buffer (car (cdr winList))))
+                  (3rdBuf (window-buffer (car (cdr (cdr winList)))))
+
+                  (split-3 
+                   (lambda(1stBuf 2ndBuf 3rdBuf split-1 split-2)
+                     "change 3 window from horizontal to vertical and vice-versa"
+                     (message "%s %s %s" 1stBuf 2ndBuf 3rdBuf)
+
+                     (delete-other-windows)
+                     (funcall split-1)
+                     (set-window-buffer nil 2ndBuf)
+                     (funcall split-2)
+                     (set-window-buffer (next-window) 3rdBuf)
+                     (other-window 2)
+                     (set-window-buffer nil 1stBuf)))         
+
+                  (split-type-1 nil)
+                  (split-type-2 nil)
+                  )
+              (if (= (window-width) (frame-width))
+                  (setq split-type-1 'split-window-horizontally 
+                        split-type-2 'split-window-vertically)
+                (setq split-type-1 'split-window-vertically  
+                      split-type-2 'split-window-horizontally))
+              (funcall split-3 1stBuf 2ndBuf 3rdBuf split-type-1 split-type-2)
 
 ))))
 
